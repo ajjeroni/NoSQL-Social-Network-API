8bc4774cd4e1109d67a7ef39690354b48c88ba1f
Adan J <adanjeronimo213@gmail.com>
2023-08-18 13:09:45 -0700

    working on user routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,9 @@
 const { User, Thought } = require('../models');
 
 module.exports = {
-  
+    getUsers(req, res) {
+        User.find()
+          .then((courses) => res.json(courses))
+          .catch((err) => res.status(500).json(err));
+      },
 };
