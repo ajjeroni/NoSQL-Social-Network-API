c90de412007cb156a76f52ae44aba811068467a4
Ajjeroni <adanjeronimo213@gmail.com>
2023-08-19 16:16:36 -0700

    working on last delete route/controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -40,20 +40,45 @@ module.exports = {
       .then((thought) => res.json(thought))
       .catch((err) => res.status(500).json(err));
   },
-  async createReaction(req, res) {
-    try {
-      const newReaction = await reactions.create(req.body);
-      console.log('json recieved')
-      if (newReaction) {
-        await Thought.findOneAndUpdate(
-          { _id: req.body.thoughtId },
-          { $push: { reactions: newReaction._id } },
-          { runValidators: true, new: true }
-        );
-      }
-      res.json('reaction created')
-    } catch (err) {
-      res.status(500).json(err);
-    }
+  // async createReaction(req, res) {
+  //   try {
+  //     const newReaction = await reactions.create(req.body);
+  //     console.log('json recieved')
+  //     if (newReaction) {
+  //       await Thought.findOneAndUpdate(
+  //         { _id: req.body.thoughtId },
+  //         { $push: { reactions: newReaction._id } },
+  //         { runValidators: true, new: true }
+  //       );
+  //     }
+  //     res.json('reaction created')
+  //   } catch (err) {
+  //     res.status(500).json(err);
+  //   }
+  // },
+  createReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $addToSet: { reactions: req.body } },
+      { new: true }
+    )
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          return res.status(404).json({ message: "No thought with this id!" });
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
   },
+  deleteReaction(req, res) {
+    Thought.findOneAndUpdate(
+      {_id: req.params.thoughtId},
+      { $pull: { reactions: req.params.reactionId } },
+      { new: true })
+    .then((reaction) => res.json(reaction))
+    .catch((err) => res.status(500).json(err));
+  }
 };
--- models/User.js
+++ models/User.js
@@ -31,7 +31,7 @@ const userSchema = new Schema(
   },
   {
     toJSON: {
-      virtual: true,
+      virtuals: true,
     },
   }
 );
