68d1ce436f5e22d18f2b43c93eb5fbbfb18af6fb
Ajjeroni <adanjeronimo213@gmail.com>
2023-08-18 18:06:26 -0700

    worked on routes/controllers

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,14 +1,43 @@
-const { Thought } = require('../models');
+const { Thought, User } = require("../models");
 
 module.exports = {
-    getThoughts(req, res) {
-        Thought.find()
-        .then((thoughts) => res.json(thoughts))
-        .catch((err) => res.status(500).json(err));
-    },
-    getSingleThought(req, res) {
-        Thought.findOne({ _id: req.params.thoughtId})
-        .then((thoughts) => res.json(thoughts))
-        .catch((err) => res.status(500).json(err));
+  getThoughts(req, res) {
+    Thought.find()
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => res.status(500).json(err));
+  },
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => res.status(500).json(err));
+  },
+  async createThought(req, res) {
+    try {
+      const newThought = await Thought.create(req.body);
+      if (newThought) {
+        await User.findOneAndUpdate(
+          { _id: req.body.userId },
+          { $addToSet: { thoughts: newThought._id } },
+          { new: true }
+        );
+      }
+      res.json('thought created')
+    } catch (err) {
+      res.status(500).json(err);
     }
+  },
+  updateThought(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $set: req.body },
+      { new: true }
+    )
+      .then((thought) => res.json(thought))
+      .catch((err) => res.status(500).json(err));
+  },
+  deleteThought(req, res) {
+    Thought.deleteOne({ _id: req.params.userId })
+      .then((thought) => res.json(thought))
+      .catch((err) => res.status(500).json(err));
+  },
 };
--- controllers/userController.js
+++ controllers/userController.js
@@ -2,12 +2,12 @@ const { User } = require("../models");
 
 module.exports = {
   getUsers(req, res) {
-    User.find()
+    User.find().populate('thoughts').populate('friends')
       .then((users) => res.json(users))
       .catch((err) => res.status(500).json(err));
   },
   getSingleUser(req, res) {
-    User.findOne({ _id: req.params.userId })
+    User.findOne({ _id: req.params.userId }).populate('thoughts').populate('friends')
       // .select('')
       .then((user) => res.json(user))
       .catch((err) => res.status(500).json(err));
@@ -28,6 +28,24 @@ module.exports = {
   },
   deleteUser(req, res) {
     User.deleteOne({ _id: req.params.userId })
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+  addFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { friends: req.params.friendId } },
+      { runValidators: true, new: true }
+    )
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+  deleteFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { friends: req.params.friendId } },
+      { runValidators: true, new: true }
+    )
       .then((user) => res.json(user))
       .catch((err) => res.status(500).json(err));
   },
